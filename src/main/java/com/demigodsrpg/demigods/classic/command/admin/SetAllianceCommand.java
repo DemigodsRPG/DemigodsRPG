e10d36295d467c7c87b26ce42a5e249cf84b8905
HmmmQuestionMark <ajchauncey@gmail.com>
2014-07-28 19:08:12 -0400

    Clean up.

--- src/main/java/com/demigodsrpg/demigods/classic/command/admin/SetAllianceCommand.java
+++ src/main/java/com/demigodsrpg/demigods/classic/command/admin/SetAllianceCommand.java
@@ -3,39 +3,25 @@ package com.demigodsrpg.demigods.classic.command.admin;
 import com.demigodsrpg.demigods.classic.DGClassic;
 import com.demigodsrpg.demigods.classic.command.type.AdminPlayerCommand;
 import com.demigodsrpg.demigods.classic.command.type.CommandResult;
-import com.demigodsrpg.demigods.classic.deity.Deity;
+import com.demigodsrpg.demigods.classic.deity.IDeity;
 import com.demigodsrpg.demigods.classic.model.PlayerModel;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class SetAllianceCommand extends AdminPlayerCommand {
     @Override
     public CommandResult onCommand(CommandSender sender, PlayerModel model, String[] args) {
         if (args.length == 3) {
-            Player p = null;
-            Deity deity = null;
-            boolean major = true;
-            try {
-                major = args[2].equalsIgnoreCase("major");
-                deity = Deity.valueOf(args[1].toUpperCase());
-                p = DGClassic.PLAYER_R.fromName(args[0]).getOfflinePlayer().getPlayer();
-            } catch (Exception ignored) {
-            }
-            if (deity == null || p == null) {
-                sender.sendMessage(ChatColor.RED + "Wrong player or deity! Please try a little harder.");
+            PlayerModel player = null;
+            IDeity.Alliance alliance = null;
+            player = DGClassic.PLAYER_R.fromName(args[0]);
+            alliance = IDeity.Alliance.valueOf(args[1].toUpperCase());
+            if (player == null || alliance == null) {
+                sender.sendMessage(ChatColor.RED + "Wrong player or alliance! Please try a little harder.");
                 return CommandResult.QUIET_ERROR;
             }
-            if (major) {
-                DGClassic.PLAYER_R.fromPlayer(p).giveMajorDeity(deity, false); // TODO This might be the first time sometimes
-                sender.sendMessage(ChatColor.YELLOW + "You added " + deity.getDeityName() + " to " + p.getName() + " as major deity.");
-                return CommandResult.SUCCESS;
-            } else {
-                DGClassic.PLAYER_R.fromPlayer(p).giveDeity(deity);
-                sender.sendMessage(ChatColor.YELLOW + "You added " + deity.getDeityName() + " to " + p.getName() + " as minor deity.");
-                return CommandResult.SUCCESS;
-            }
-
+            player.setAlliance(alliance);
+            sender.sendMessage(ChatColor.YELLOW + player.getLastKnownName() + " has been set to " + alliance.name() + ".");
         }
         return CommandResult.NOT_ENOUGH_ARGS;
     }
