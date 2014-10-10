dc25f714499b959a8a96a60ed86e3d2576d1b0e2
HmmmQuestionMark <ajchauncey@gmail.com>
2014-10-10 11:12:58 -0400

    Cleaned up and documented the forsake command. Thanks @TheFatDemon

--- src/main/java/com/demigodsrpg/demigods/classic/command/admin/ForsakeCommand.java
+++ src/main/java/com/demigodsrpg/demigods/classic/command/admin/ForsakeCommand.java
@@ -9,83 +9,108 @@ import com.demigodsrpg.demigods.classic.model.PlayerModel;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class ForsakeCommand extends BaseCommand {
     @Override
     protected CommandResult onCommand(CommandSender sender, Command command, String[] args) {
+        // Check for no args
         if (args.length == 0) {
-            sender.sendMessage(ChatColor.YELLOW + "To forsake all do /forsake all");
+            sender.sendMessage(ChatColor.YELLOW + "To forsake all, do /forsake all");
             return CommandResult.SUCCESS;
-        }else if(args.length == 1){
-            if (!(sender instanceof Player)){
-                sender.sendMessage(ChatColor.DARK_RED + "You are not allowed to do this.");
+        }
+        // Check for only 1 arg
+        else if (args.length == 1) {
+            // This subcommand doesn't allow the console
+            if (sender instanceof ConsoleCommandSender) {
                 return CommandResult.PLAYER_ONLY;
             }
+
+            // Get the player object
             Player player = (Player) sender;
-            PlayerModel playerModel;
-            try {
-                playerModel = DGClassic.PLAYER_R.fromPlayer(player);
-            }catch (Exception ignored){
-                sender.sendMessage(ChatColor.DARK_RED + "Wrong player or deity! Please try a little harder.");
+            PlayerModel playerModel = DGClassic.PLAYER_R.fromPlayer(player);
+
+            // Check that the player model is not null
+            if (playerModel == null) {
+                sender.sendMessage(ChatColor.DARK_RED + "Something is wrong with your player data. Try to re-log.");
                 return CommandResult.QUIET_ERROR;
             }
-            if (args[0].equalsIgnoreCase("all")){
-                for (String deityName : playerModel.getAllDeities()){
-                    Deity deity;
-                    try {
-                        deity = Deity.valueOf(deityName.toUpperCase());
-                    }catch (Exception ignored){
-                        sender.sendMessage(ChatColor.RED + "Wrong player or deity! Please try a little harder.");
+
+            // Do '/forsake all'
+            if ("all".equalsIgnoreCase(args[0])) {
+                // Loop over the player's deities
+                for (String deityName : playerModel.getAllDeities()) {
+                    // Get the deity object
+                    Deity deity = Deity.valueOf(deityName.toUpperCase());
+                    if (deity == null) {
+                        sender.sendMessage(ChatColor.RED + deityName + " does not exist, something is wrong with the player data...");
                         return CommandResult.QUIET_ERROR;
                     }
-                    if (playerModel.getMajorDeity().equals(deity)){
+
+                    // Check if the deity is major or minor
+                    if (playerModel.getMajorDeity().equals(deity)) {
                         playerModel.setMajorDeity(Deity.HUMAN);
                         sender.sendMessage(ChatColor.YELLOW + "You have forsaken " + deity.getDeityName());
-                    }else {
+                    } else {
                         playerModel.removeContractedDeity(deity);
                         sender.sendMessage(ChatColor.YELLOW + "You have forsaken " + deity.getDeityName());
                     }
                 }
+
+                // Set the alliance to neutral
+                playerModel.setAlliance(IDeity.Alliance.NEUTRAL);
                 return CommandResult.SUCCESS;
-            }else {
+            } else {
+                // TODO Check for individual deity forsake requests.
                 sender.sendMessage(ChatColor.YELLOW + "To forsake all do /forsake all");
                 return CommandResult.SUCCESS;
             }
-        }else if (args.length == 2){
-            if (!sender.hasPermission("demigods.admin.forsake")){
+        }
+        // Handle admin command to forsake other players
+        else if (args.length == 2) {
+            // Check if the sender has permission to do the forsaking.
+            if (!sender.hasPermission("demigods.admin.forsake")) {
                 return CommandResult.NO_PERMISSIONS;
             }
-            if (args[1].equalsIgnoreCase("all")){
-                PlayerModel playerModel;
-                try {
-                    playerModel = DGClassic.PLAYER_R.fromName(args[0]);
-                }catch (Exception ingnored){
-                    sender.sendMessage(ChatColor.DARK_RED + "Wrong player! Please try a little harder.");
+
+            // Handle '/forsake <player> all' subcommand
+            if ("all".equalsIgnoreCase(args[1])) {
+                // Get the target's player model
+                PlayerModel playerModel = DGClassic.PLAYER_R.fromName(args[0]);
+                if (playerModel == null) {
+                    sender.sendMessage(ChatColor.DARK_RED + "No such player! Please try a little harder.");
                     return CommandResult.QUIET_ERROR;
                 }
-                playerModel.setAlliance(IDeity.Alliance.NEUTRAL);
-                for (String deityName : playerModel.getAllDeities()){
-                    Deity deity;
-                    try {
-                        deity = Deity.valueOf(deityName);
-                    }catch (Exception ignored){
+
+                // Loop over the target's deities
+                for (String deityName : playerModel.getAllDeities()) {
+                    // Get the deity from the name
+                    Deity deity = Deity.valueOf(deityName);
+                    if (deity == null) {
                         continue;
                     }
-                    if (playerModel.getMajorDeity().equals(deity)){
+
+                    // Check if the deity is major or minor
+                    if (playerModel.getMajorDeity().equals(deity)) {
                         playerModel.setMajorDeity(Deity.HUMAN);
-                    }else {
+                    } else {
                         playerModel.removeContractedDeity(deity);
                     }
                 }
-                sender.sendMessage(ChatColor.YELLOW + "You have removed all Deities from " + args[0]);
+
+                // Set the target's alliance to neutral.
+                playerModel.setAlliance(IDeity.Alliance.NEUTRAL);
+                sender.sendMessage(ChatColor.YELLOW + "You have removed all deities from " + playerModel.getLastKnownName());
+
                 return CommandResult.SUCCESS;
-            }else{
-                sender.sendMessage(ChatColor.YELLOW + "Invalid Usage");
-                sender.sendMessage(ChatColor.YELLOW + "/forsake <player> all");
+            } else {
+                // TODO Check for individual deity forsake requests.
                 return CommandResult.INVALID_SYNTAX;
             }
         }
+
+        // Something went wrong...
         return CommandResult.QUIET_ERROR;
     }
 }
